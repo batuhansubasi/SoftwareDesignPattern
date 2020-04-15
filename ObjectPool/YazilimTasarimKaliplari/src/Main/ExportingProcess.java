2f4e9ee54cac13e2d0c74076cf04f0bb233d1bd2
Batuhan <30631029+batuhansubasi@users.noreply.github.com>
2020-04-15 03:11:26 +0300

    4.Hafta

--- ObjectPool/YazilimTasarimKaliplari/src/Main/ExportingProcess.java
+++ ObjectPool/YazilimTasarimKaliplari/src/Main/ExportingProcess.java
@@ -1,5 +1,15 @@
 package Main;
 
-public class ExportingProcess {
-
+public class ExportingProcess {  
+ private long processNo;  
+  
+    public ExportingProcess(long processNo)  {  
+         this.processNo = processNo;    
+		 
+      System.out.println("İsleme numarası olan nesne " + processNo + " yaratildi.");  
+     }  
+     
+    public long getProcessNo() {  
+        return processNo;  
+    }  
 }
--- ObjectPool/YazilimTasarimKaliplari/src/Main/ExportingTask.java
+++ ObjectPool/YazilimTasarimKaliplari/src/Main/ExportingTask.java
@@ -1,5 +1,22 @@
 package Main;
 
-public class ExportingTask {
-
-}
+	public class ExportingTask implements Runnable {  
+        private ObjectPool<ExportingProcess> pool;  
+        private int threadNo;  
+        public ExportingTask(ObjectPool<ExportingProcess> pool, int threadNo){  
+            this.pool = pool;  
+            this.threadNo = threadNo;  
+        }  
+      
+        public void run() {  
+            ExportingProcess exportingProcess = pool.borrowObject();  
+            System.out.println("Thread " + threadNo + ": İsleme numarası olan nesne. "  
+                    + exportingProcess.getProcessNo() + "alındı.");  
+ 
+            pool.returnObject(exportingProcess);  
+  
+            System.out.println("Thread " + threadNo +": İsleme numarası olan nesne "  
+                   + exportingProcess.getProcessNo() + " geri döndürüldü.");  
+        }  
+ 
+    }
