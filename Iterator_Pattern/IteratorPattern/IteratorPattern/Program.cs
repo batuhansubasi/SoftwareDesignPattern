f323c8d039de798a93cbccae3f4f92a56f54bb9d
Batuhan <30631029+batuhansubasi@users.noreply.github.com>
2020-05-13 04:24:11 +0300

    6.Hafta

---
+++ Iterator_Pattern/IteratorPattern/IteratorPattern/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+// 160202091 - Batuhan Subaşı
+
+// Bu projede kanallar arasında gezinme işlemini soyutlaştırmak icin kullanıldı.
+// IKanalIterator: Koleksiyon elemanlarını almaya yarayan islemler yapıyor.
+// IKanalAggregate: Iterator tipinden nesne alıyor.
+// KanalConcreteAggregate:  KanalConcreteIterator ismindeki gerçek iterator nesnesi oluşturuyor, kanal koleksiyonuna sahip.
+// KanalConcreteIterator: Koleksiyon elemanlarını elde ediyor.
+
+
+namespace IteratorPattern
+{
+    //Tvkanal sınıfı
+    public class TVKnali
+    {
+        public static int ObjectCounter = 0;
+        public string KanalAdi { get; set; }
+
+    }
+
+    //İnterface Kanal İterator
+    public interface IKanalIterator
+    {
+        TVKnali İlkKanal();
+        TVKnali SonrakiKanal();
+        TVKnali GecerliKanal();
+        bool bittiMi();
+    }
+
+    //Interface Kanal Aggregate
+    public interface IKanalAggregate
+    {
+        IKanalIterator getIterator();
+    }
+
+    //Kanal Concreate Aggregate Sınıfı
+    public class KanalConcreteAggregate : IKanalAggregate
+    {
+        private List<TVKnali> _kanalListesi = new List<TVKnali>();
+        public int kanalSayisi
+        {
+            get { return _kanalListesi.Count; }
+        }
+        public void Ekle(TVKnali t)
+        {
+            _kanalListesi.Add(t);
+        }
+        public TVKnali GetItem(int deger)
+        {
+            return _kanalListesi[deger];
+        }
+        public IKanalIterator getIterator()
+        {
+            return new KanalConcreteIterator(this);
+        }
+    }
+
+    //Kanal Concrete Iterator 
+    public class KanalConcreteIterator : IKanalIterator
+    {
+        private KanalConcreteAggregate kanallar;
+        private int deger;
+        public KanalConcreteIterator(KanalConcreteAggregate kanal)
+        {
+            kanallar = kanal;
+        }
+        public TVKnali İlkKanal()
+        {
+            deger = 0;
+            return kanallar.GetItem(deger);
+        }
+        public bool bittiMi()
+        {
+            return deger < kanallar.kanalSayisi;
+        }
+        public TVKnali GecerliKanal()
+        {
+            return kanallar.GetItem(deger);
+        }
+        public TVKnali SonrakiKanal()
+        {
+            deger++;
+            if (bittiMi())
+            {
+                return kanallar.GetItem(deger);
+            }
+            else
+                return null;
+        }
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Factory factory = new Factory();
+                KanalConcreteAggregate kca = new KanalConcreteAggregate();
+                kca.Ekle(factory.getTVKnali("Kanal D"));
+                kca.Ekle(factory.getTVKnali("ATV"));
+                kca.Ekle(factory.getTVKnali("Show TV"));
+                kca.Ekle(factory.getTVKnali("A Haber"));
+                kca.Ekle(factory.getTVKnali("Kanal7"));
+                IKanalIterator kanalI = kca.getIterator();
+                string kanallar = "";
+                kanalI.İlkKanal();
+                while (kanalI.bittiMi())
+                {
+                    kanallar += kanalI.GecerliKanal().KanalAdi + Environment.NewLine;
+                    kanalI.SonrakiKanal();
+                }
+                Console.WriteLine(kanallar);
+                Console.ReadKey();
+            }
+        }
+
+        class Factory
+        {
+            private static int havuzKapasite = 2;
+            private static readonly Queue objPool = new Queue(havuzKapasite);
+
+            public TVKnali getTVKnali(String kanaladi)
+            {
+                TVKnali oTVKnali;
+                if (TVKnali.ObjectCounter >= havuzKapasite && objPool.Count > 0)
+                {
+                    oTVKnali = HavuzdanGetir(kanaladi);
+                }
+                else
+                {
+                    oTVKnali = GetNewTvKnali(kanaladi);
+                }
+                return oTVKnali;
+            }
+
+            private TVKnali GetNewTvKnali(String kanaladi)
+            {
+                //yeni kanal üret
+                TVKnali kanal = new TVKnali { KanalAdi = kanaladi };
+                objPool.Enqueue(kanal);
+                return kanal;
+            }
+
+            protected TVKnali HavuzdanGetir(String kanaladi)
+            {
+                TVKnali kanal;
+
+                if (objPool.Count > 0)
+                {
+                    kanal = (TVKnali)objPool.Dequeue();
+                    TVKnali.ObjectCounter--;
+                }
+                else
+                {
+                    kanal = new TVKnali { KanalAdi = kanaladi };
+                }
+                return kanal;
+            }
+
+        }
+
+    }
+}
---
+++ Observer_Pattern/ObserverPattern/ObserverPattern/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 160202091 - Batuhan Subasi
+
+// Bu çalışmadaki patternin amacı: Vize ve Final bilgi sistemi açıldığında, sadece bu konu ile ilgilenen öğretmen, öğrenci ve dekan nesnelerine bildirim atılması. Abone olmayanların kayıt dışı olması.
+// Concrete Subscribers: Öğretmen, Öğrenci, Dekan
+// Publisher: VizeFinal
+// Observer: Öğretmen, öğrenci ve dekanların soyut gözlemcisi.
+
+
+namespace ObserverPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VizeFinal not = new VizeFinal();
+
+            Ögretmen ogretmen = new Ögretmen();
+            Ögrenci öğrenci = new Ögrenci();
+            Dekan dekan = new Dekan();
+
+            not.AboneEkle(ogretmen);
+            not.AboneEkle(öğrenci);
+            not.AboneEkle(dekan);
+
+            not.DersAdi = "Bulanık Mantık";
+            not.HocaAdi = "Batuhan Subasi";
+            not.Kredisi = 5;
+            not.NotGirildimi = true;
+
+            not.AboneCikar(ogretmen);
+
+            not.DersAdi = "Bitirme Projesi";
+            not.HocaAdi = "Alev Mutlu";
+            not.Kredisi = 4;
+            not.NotGirildimi = true;
+
+            Console.ReadKey(true);
+        }
+    }
+    //Observer arayüzü bir çok tipin abone olması için
+    abstract public class Observer
+    {
+        public abstract void Güncelle();
+    }
+
+    //Concrete sınıflar Takip edecekler
+    public class Ögretmen : Observer
+    {
+        public override void Güncelle()
+        {
+            Console.WriteLine("Öğretmenin Eriştiği Yer");
+        }
+    }
+    public class Ögrenci : Observer
+    {
+        public override void Güncelle()
+        {
+            Console.WriteLine("Öğrencinin Eriştiği Yer");
+        }
+    }
+    public class Dekan : Observer
+    {
+        public override void Güncelle()
+        {
+            Console.WriteLine("Dekanın Eriştiği Yer");
+        }
+    }
+    public class VizeFinal
+    {
+        public string DersAdi { get; set; }
+        public string HocaAdi { get; set; }
+        public int Kredisi { get; set; }
+        bool notGirildimi;
+        public bool NotGirildimi
+        {
+            get { return notGirildimi; }
+            set
+            {
+                if (value == true)
+                {
+                    HaberVer();
+                    notGirildimi = value;
+                }
+                else
+                    notGirildimi = value;
+            }
+        }
+        //Subject nesnesi kendisine abone olan gözlemcileri bu koleksiyonda tutacaktır.
+        List<Observer> Gozlemciler;
+        public VizeFinal()
+        {
+            this.Gozlemciler = new List<Observer>();
+        }
+        //Gözlemci ekle
+        public void AboneEkle(Observer observer)
+        {
+            Gozlemciler.Add(observer);
+        }
+        //Gözlemci çıkar
+        public void AboneCikar(Observer observer)
+        {
+            Gozlemciler.Remove(observer);
+        }
+        //Herhangi bir güncelleme olursa ilgili gözlemcilere haber verecek metodumuzdur.
+        public void HaberVer()
+        {
+            Gozlemciler.ForEach(g =>
+            {
+                g.Güncelle();
+            });
+        }
+
+    }
+
+}
